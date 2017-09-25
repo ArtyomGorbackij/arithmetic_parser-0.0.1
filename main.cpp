54d787ca4cb29501fd44fd690e8c3864b8058d31
ArtyomGorbackij <31548827+ArtyomGorbackij@users.noreply.github.com>
2017-09-25 15:15:13 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+int main() {
+    using namespace std;
+int a,b,c;
+  char f;
+
+    cin >> a>>f>>b;
+    switch (f) {
+        case '+':
+            c = a + b;
+            cout << (c);
+            break;
+        case '-':
+            c = a - b;
+            cout << (c);
+            break;
+        case '*':
+            c = a * b;
+            cout << (c);
+            break;
+        case '/':
+            c = a / b;
+            cout << (c);
+            break;
+        case '%':
+            c = a % b;
+            cout << (c);
+            break;
+        case '&':
+            c = a & b;
+            cout << (c);
+            break;
+        case '|':
+            c = a | b;
+            cout << (c);
+            break;
+        case '^':
+            c = a ^ b;
+            cout << (c);
+            break;
+        case '<':
+            c = a << b;
+            cout << (c);
+        case '>':
+            c = a >> b;
+            cout << (c);
+            break;
+    }
+            return 0;
+
+}
