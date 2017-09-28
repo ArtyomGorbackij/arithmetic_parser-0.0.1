9b108625fb61e63eb6876b28ef4b954224261061
ArtyomGorbackij <31548827+ArtyomGorbackij@users.noreply.github.com>
2017-09-28 22:47:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,19 +4,22 @@ int main() {
 int a,b;
 char f,g;
 
-    cin >> a>>f>>b;
-    if (f == '+'){cout<<a+b;}
-    else if (f== '-'){cout<<a-b;}
-    else if (f== '/'){cout<<(double)a/b;}
-    else if (f== '*'){cout<<a*b;}
-    else if (f== '%'){cout<<a%b;}
-    else if (f== '|'){cout<<(a|b);}
-    else if (f== '^'){cout<<(a^b);}
-    else if (f== '&'){cout<<(a&b);}
-    else if (f== '>'){cin>>g;
-    if (f==g){cout<<(a>>b);}}
-    else if (f== '<'){{cin>>g;
-    if (f==g){cout<<(a<<b);}}}
+    cin >> a>>f;
+    if (f == '+'){cin>>b;
+        cout<<a+b;}
+    else if (f== '-'){cin>>b;cout<<a-b;}
+    else if (f== '/'){cin>>b;cout<<(double)a/b;}
+    else if (f== '*'){cin>>b;cout<<a*b;}
+    else if (f== '%'){cin>>b;cout<<a%b;}
+    else if (f== '|'){cin>>b;cout<<(a|b);}
+    else if (f== '^'){cin>>b;cout<<(a^b);}
+    else if (f== '&'){cin>>b;cout<<(a&b);}
+    else if (f== '>'){cin>>g>>b;
+    if (f==g){cout<<(a>>b);}
+        else cout<<("Error!");}
+    else if (f== '<'){{cin>>g>>b;
+    if (f==g){cout<<(a<<b);}
+        else{ cout<<("Error!");}}}
             return 0;
 
 }
